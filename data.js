888595f36babc04c6f3e5e1109c15862a6d2aca2
Sanju Kamat <sanjukamat123@gmail.com>
2023-09-26 23:21:17 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,337 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-swimming-pool",
+      "name": "Swimming Pool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8034879100333789,
+        "pitch": -0.14865827930499975,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20168183782342908,
+          "pitch": 0.08535442396084392,
+          "rotation": 0,
+          "target": "2-swimming-pool-"
+        },
+        {
+          "yaw": 1.3302900228355607,
+          "pitch": 0.06145499571127111,
+          "rotation": 0,
+          "target": "3-swimming-pool-"
+        },
+        {
+          "yaw": -3.125809299247635,
+          "pitch": 0.0254962380979471,
+          "rotation": 0,
+          "target": "4-swimming-pool-"
+        },
+        {
+          "yaw": -2.3014319833377286,
+          "pitch": 0.05318123307059608,
+          "rotation": 0,
+          "target": "5-swimming-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-swimming-pool-",
+      "name": "Swimming Pool ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.1717601193607656,
+        "pitch": -0.06026506844662194,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1917228185461664,
+          "pitch": 0.0517350544176729,
+          "rotation": 0,
+          "target": "3-swimming-pool-"
+        },
+        {
+          "yaw": 0.1622408336512109,
+          "pitch": 0.07508253470320447,
+          "rotation": 0,
+          "target": "2-swimming-pool-"
+        },
+        {
+          "yaw": -3.1301721731287007,
+          "pitch": 0.03392088414868866,
+          "rotation": 0,
+          "target": "4-swimming-pool-"
+        },
+        {
+          "yaw": -2.1706474753168408,
+          "pitch": 0.1072609730138705,
+          "rotation": 0,
+          "target": "5-swimming-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-swimming-pool-",
+      "name": "Swimming Pool ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.390889502248184,
+        "pitch": -0.05732173072224711,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07906017415712796,
+          "pitch": 0.039822059370152374,
+          "rotation": 0,
+          "target": "0-swimming-pool"
+        },
+        {
+          "yaw": -1.0493198655872131,
+          "pitch": 0.047981937366500915,
+          "rotation": 0,
+          "target": "3-swimming-pool-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-swimming-pool-",
+      "name": "Swimming Pool ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.3615084073049246,
+        "pitch": -0.06396496139450925,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8862840094231306,
+          "pitch": 0.1032394164129844,
+          "rotation": 0,
+          "target": "1-swimming-pool-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-swimming-pool-",
+      "name": "Swimming Pool ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8021577069430208,
+        "pitch": -0.21850146786361435,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11652340249662174,
+          "pitch": 0.07234774830341628,
+          "rotation": 0,
+          "target": "3-swimming-pool-"
+        },
+        {
+          "yaw": -0.8721338724366987,
+          "pitch": 0.11625703947090216,
+          "rotation": 0,
+          "target": "0-swimming-pool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-swimming-pool",
+      "name": "Swimming Pool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.9371416926048806,
+        "pitch": 0.31471947346850015,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2697135977845466,
+          "pitch": 0.09763191995733145,
+          "rotation": 0,
+          "target": "6-swimming-pool"
+        },
+        {
+          "yaw": -1.302925732613737,
+          "pitch": 0.027319947830861224,
+          "rotation": 0,
+          "target": "0-swimming-pool"
+        },
+        {
+          "yaw": -0.2423891786557153,
+          "pitch": 0.06507081383121971,
+          "rotation": 0,
+          "target": "3-swimming-pool-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-swimming-pool",
+      "name": "Swimming Pool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8901642433180292,
+        "pitch": -0.07061310326662706,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.639014151196708,
+          "pitch": 0.02181289575645451,
+          "rotation": 0,
+          "target": "0-swimming-pool"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Swimming Pool",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
